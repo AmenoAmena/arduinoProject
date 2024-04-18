aab779e490e78f643a17f17a6ff313b31f1f7fb9
AmenoAmena <ardacavusoglu666@gmail.com>
2024-04-18 22:54:22 +0300

    crud completed now

--- BaseApp/views.py
+++ BaseApp/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render,redirect
 from .forms import itemForm
 from .models import sensorImage, item
 
-def index(request):
-    items = item.objects.all()
-    return render(request, 'BaseApp/index.html', {
-        'items': items
-    })
-
+def index(request,pk=0):
+    if request.method == "GET":
+        items = item.objects.all()
+        return render(request, 'BaseApp/index.html', {
+            'items': items
+        })
+    else:
+        if pk == 0:
+            form = itemForm(request.POST)
+        else:
+            item_obj = item.objects.get(pk=id)
+            form = itemForm(request.POST,instance=item_obj)
+            return redirect("add")
+        if form.is_valid():
+            form.save()
+        return redirect("index")
 def add(request):
     if request.method == 'POST':
         form = itemForm(request.POST)
@@ -17,5 +27,24 @@ def add(request):
     else:
         form = itemForm()
     return render(request, 'BaseApp/add.html', {
+        'form': form
+    })
+
+def delete(request,pk):
+    sensor = item.objects.get(pk=pk)
+    sensor.delete()
+    return redirect('index')
+
+def update_item(request, pk):
+    if request.method == "POST":
+        item_obj = item.objects.get(pk=pk)
+        form = itemForm(request.POST, instance=item_obj)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        item_obj = item.objects.get(pk=pk)
+        form = itemForm(instance=item_obj)
+    return render(request, 'BaseApp/update.html', {
         'form': form
     })
