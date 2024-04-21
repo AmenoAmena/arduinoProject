1e01b14d7633d26e1cc0911096d5c6ae8974ceda
AmenoAmena <ardacavusoglu666@gmail.com>
2024-04-21 14:17:01 +0300

    index css gelmeli kalanı bitti

--- BaseApp/forms.py
+++ BaseApp/forms.py
@@ -34,7 +34,21 @@ class itemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Customize the representation of the image field choices
+
         self.fields['image'].queryset = sensorImage.objects.all()
         self.fields['image'].label_from_instance = lambda obj: obj.name
         self.fields['image'].empty_label = 'Model Seçiniz'
+
+        self.fields['name'].widget.attrs['autocomplete'] = 'off'
+        self.fields['image'].widget.attrs['autocomplete'] = 'off'
+        self.fields['number'].widget.attrs['autocomplete'] = 'off'
+        
+        placeholders = {
+            'name': 'Sensör ismi',
+            'image': 'Sensör tipi',
+            'number': 'Sensör sayısı'
+        }
+
+        for field_name, placeholder_text in placeholders.items():
+            self.fields[field_name].widget.attrs['placeholder'] = placeholder_text
+
